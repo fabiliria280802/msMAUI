0c6c26856a4723452e08dda939b269f6fda1ebd1
Fabiana <fabiana.liria@udla.edu.ec>
2022-11-18 14:26:20 -0500

    Agregar archivos de proyecto.

---
+++ msMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace msMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ msMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace msMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
