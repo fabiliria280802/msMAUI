2c37d9b17ed2034aa8fa680ea759d4342f10cd6e
Fabiana <fabiana.liria@udla.edu.ec>
2022-12-09 18:13:20 -0500

    Atributos de movie

--- msMAUI/Models/AllMovies.cs
+++ msMAUI/Models/AllMovies.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Android.Provider.ContactsContract.CommonDataKinds;
 
 namespace msMAUI.Models
 {
     internal class AllMovies
     {
+        public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
     }
 }
---
+++ msMAUI/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msMAUI.Models
+{
+    internal class Movie
+    {
+        public string Filename { get; set; }
+        public string title { get; set; }
+
+        public DateTime time { get; set; }
+
+        public int year { get; set; }
+
+        public string director { get; set; }
+
+        public bool shortFilm { get; set; }
+        public decimal income { get; set; }
+
+        public string distributor { get; set; }
+        public string gender { get; set; }
+        public string classification { get; set; }
+        public string synopsis { get; set; }
+    }
+}
