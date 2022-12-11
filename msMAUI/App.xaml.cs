25b808c3965b4502b62a4bad3eba587faee4ea3a
mateoudla <117088528+mateoudla@users.noreply.github.com>
2022-12-11 01:41:16 -0500

    Soluciones modernas para problemas modernos ;)

--- msMAUI/App.xaml.cs
+++ msMAUI/App.xaml.cs
@@ -2,10 +2,18 @@
 
 public partial class App : Application
 {
+<<<<<<< Updated upstream
 	public App()
 	{
 		InitializeComponent();
 
 		MainPage = new AppShell();
 	}
+=======
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new Views.Bienvenida();
+    }
+>>>>>>> Stashed changes
 }
---
+++ msMAUI/Views/Bienvenida.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace msMAUI.Views;
+
+public partial class Bienvenida : ContentPage
+{
+	public Bienvenida()
+	{
+		InitializeComponent();
+	}
+
+    void OnCounterClicked(System.Object sender, System.EventArgs e)
+    {
+        Navigation.PushAsync(new AppShell());
+        Application.Current.MainPage = new AppShell();
+    }
+}
