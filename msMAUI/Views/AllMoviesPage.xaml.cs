ff6c9edd7e1ff1c6c7e9bcc9f91fd4949d76d157
Fabiana <fabiana.liria@udla.edu.ec>
2022-12-11 23:22:46 -0500

    -Resuelto biblioteca

--- msMAUI/Views/AllMoviesPage.xaml.cs
+++ msMAUI/Views/AllMoviesPage.xaml.cs
@@ -35,18 +35,19 @@ public partial class AllMoviesPage : ContentPage
                 var movie = (Models.Movie)e.CurrentSelection[0];
 
                 // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
-                await Shell.Current.GoToAsync($"{nameof(MoviePage)}?{nameof(MoviePage.ItemId)}={movie.Filename}");
+                //await Shell.Current.GoToAsync($"{nameof(MoviePage)}?{nameof(MoviePage.ItemId)}={movie.Filename}");
                 //codigo que no funcionan
+                await Navigation.PushAsync(new MoviePage());
                 //await Navigation.PushAsync(nameof(MoviePage.ItemId)=movie.Filename);
                 // Unselect the UI
                 moviesCollection.SelectedItem = null;
-                    //Shell.Current.GoToAsync($"{nameof(MoviePage)}?{nameof(MoviePage.ItemId)}={movie.Filename}");
+                //Shell.Current.GoToAsync($"{nameof(MoviePage)}?{nameof(MoviePage.ItemId)}={movie.Filename}");
             }
-            else
+            /*else
             {
                 await DisplayAlert("Alerta: ", "No entra al if", "Ok");
 
-            }
+            }*/
         }
         catch (InvalidCastException ice)
         {
