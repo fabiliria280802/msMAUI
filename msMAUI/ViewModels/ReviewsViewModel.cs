c31a4a53ef70125abb35ad4d5fdd231e06263f44
mateoudla <117088528+mateoudla@users.noreply.github.com>
2023-01-31 20:47:54 -0500

    Entender es mi pasión

--- msMAUI/ViewModels/ReviewsViewModel.cs
+++ msMAUI/ViewModels/ReviewsViewModel.cs
@@ -8,21 +8,29 @@ namespace msMAUI.ViewModels
 {
     public class ReviewsViewModel
     {
+        // Propiedad privada que contiene una colección de objetos `Review`
         private ObservableCollection<Review> _reviews;
 
+        // Propiedad pública para acceder a la colección de objetos `Review`
         public ObservableCollection<Review> Reviews
         {
             get { return _reviews; }
             set { _reviews = value; }
         }
 
+        // Método asíncrono para obtener la información de los comentarios de la API
         public async Task GetReviewsAsync()
         {
+            // Crea una nueva instancia de `HttpClient` para realizar la petición
             var client = new HttpClient();
+
+            // Realiza una llamada GET a la API y obtiene la respuesta en formato JSON
             var json = await client.GetStringAsync("https://jsonplaceholder.typicode.com/comments");
 
+            // Deserializa la respuesta JSON y la almacena en una nueva colección de objetos `Review`
             var reviews = JsonConvert.DeserializeObject<ObservableCollection<Review>>(json);
 
+            // Almacena la nueva colección en la propiedad `Reviews`
             Reviews = reviews;
         }
     }
